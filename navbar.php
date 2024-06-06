44b6ac2b5bb70d0eeb03b7fa9b2073da41b4767a
SaiPiyusha23 <saipiyushaparripati@gmail.com>
2024-06-06 15:09:14 +0530

    Add files via upload

---
+++ navbar.php
@@ -0,0 +1,29 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+    <div class="container">
+        <a class="navbar-brand" href="#">Movie Library Web</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="welcome.php">Home</a>
+                </li>
+            </ul>
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a class="nav-link btn btn-outline-success mx-2" href="signup.php">Signup</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link btn btn-outline-primary mx-2" href="login.php">Login</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link btn btn-outline-danger mx-2" href="logout.php">Logout</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link btn btn-outline-info mx-2" href="mylist.php">My List</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
